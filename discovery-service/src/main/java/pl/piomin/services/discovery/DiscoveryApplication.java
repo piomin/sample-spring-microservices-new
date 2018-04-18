bf76a2df8bb2cda6ad741061c025f204f3fef3c9
piotr.minkowski <piotr.minkowski@gmail.com>
2018-04-18 17:18:32 +0200

    adding config and discovery

---
+++ discovery-service/src/main/java/pl/piomin/services/discovery/DiscoveryApplication.java
@@ -0,0 +1,15 @@
+package pl.piomin.services.discovery;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(DiscoveryApplication.class).run(args);
+	}
+
+}
