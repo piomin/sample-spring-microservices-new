fd2499e440f0d85de1c5cc2bd143570890ded48b
Piotr Mińkowski <piotr_minkowski@wp.pl>
2018-07-29 23:52:24 +0200

    changing gateway

--- gateway-service/src/main/java/pl/piomin/services/gateway/GatewayApplication.java
+++ gateway-service/src/main/java/pl/piomin/services/gateway/GatewayApplication.java
@@ -3,9 +3,14 @@ package pl.piomin.services.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableZuulProxy
+@EnableSwagger2
 public class GatewayApplication {
 
 	
