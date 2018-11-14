255e09eed90e2533ca6f1f11cb3f726e65fa8278
piotr.minkowski <piotr.minkowski@gmail.com>
2018-11-14 16:45:12 +0100

    enabling nacos on zuul gateway

--- proxy-service/src/main/java/pl/piomin/service/proxy/ProxyApplication.java
+++ proxy-service/src/main/java/pl/piomin/service/proxy/ProxyApplication.java
@@ -2,11 +2,13 @@ package pl.piomin.service.proxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableZuulProxy
 @EnableSwagger2
 public class ProxyApplication {
