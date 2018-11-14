e49f14fa1ba5c5fc8adfd3a42cba17f35be6b99b
piotr.minkowski <piotr.minkowski@gmail.com>
2018-11-14 15:52:53 +0100

    enabling nacos config for department and organization services

--- employee-service/src/main/java/pl/piomin/services/employee/EmployeeApplication.java
+++ employee-service/src/main/java/pl/piomin/services/employee/EmployeeApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
-import pl.piomin.services.employee.model.Employee;
-import pl.piomin.services.employee.repository.EmployeeRepository;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -32,21 +30,5 @@ public class EmployeeApplication {
 				.build()
 				.apiInfo(new ApiInfoBuilder().version("1.0").title("Employee API").description("Documentation Employee API v1.0").build());
 	}
-	
-//	@Bean
-//	EmployeeRepository repository() {
-//		EmployeeRepository repository = new EmployeeRepository();
-//		repository.add(new Employee(1L, 1L, "John Smith", 34, "Analyst"));
-//		repository.add(new Employee(1L, 1L, "Darren Hamilton", 37, "Manager"));
-//		repository.add(new Employee(1L, 1L, "Tom Scott", 26, "Developer"));
-//		repository.add(new Employee(1L, 2L, "Anna London", 39, "Analyst"));
-//		repository.add(new Employee(1L, 2L, "Patrick Dempsey", 27, "Developer"));
-//		repository.add(new Employee(2L, 3L, "Kevin Price", 38, "Developer"));
-//		repository.add(new Employee(2L, 3L, "Ian Scott", 34, "Developer"));
-//		repository.add(new Employee(2L, 3L, "Andrew Campton", 30, "Manager"));
-//		repository.add(new Employee(2L, 4L, "Steve Franklin", 25, "Developer"));
-//		repository.add(new Employee(2L, 4L, "Elisabeth Smith", 30, "Developer"));
-//		return repository;
-//	}
 	
 }
