872e93775870e6c49fe3af3a27b4d673c689e632
liaow <cs.liaow@gmail.com>
2020-04-20 09:35:19 +0800

    upgrade springdoc to 1.3.4 [refer origin commit]https://github.com/springdoc/springdoc-openapi/commit/f4943466e90884148ed9bc8920675ab6c6b8fc01

--- gateway-service/src/main/java/pl/piomin/services/gateway/GatewayApplication.java
+++ gateway-service/src/main/java/pl/piomin/services/gateway/GatewayApplication.java
@@ -1,15 +1,12 @@
 package pl.piomin.services.gateway;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.PathItem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springdoc.core.GroupedOpenApi;
-import org.springdoc.core.customizers.OpenApiCustomiser;
+import org.springdoc.core.SwaggerUiConfigProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteDefinition;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.context.annotation.Bean;
@@ -31,11 +28,12 @@ public class GatewayApplication {
 	RouteDefinitionLocator locator;
 
 	@Bean
-	public List<GroupedOpenApi> apis() {
+	public List<GroupedOpenApi> apis(SwaggerUiConfigProperties swaggerUiConfigProperties) {
 		List<GroupedOpenApi> groups = new ArrayList<>();
 		List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
 		definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-service")).forEach(routeDefinition -> {
 			String name = routeDefinition.getId().replaceAll("-service", "");
+            swaggerUiConfigProperties.addGroup(name);
 			GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").setGroup(name).build();
 		});
 		return groups;
