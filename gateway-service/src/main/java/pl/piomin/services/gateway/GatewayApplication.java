c58a1e7b1b97fe4623ace6ee7a3ff7407dacd010
piomin <piotr.minkowski@gmail.com>
2024-04-30 15:06:14 +0200

    #109 Fix springdoc OpenAPI UI display

--- gateway-service/src/main/java/pl/piomin/services/gateway/GatewayApplication.java
+++ gateway-service/src/main/java/pl/piomin/services/gateway/GatewayApplication.java
@@ -2,17 +2,19 @@ package pl.piomin.services.gateway;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springdoc.core.models.GroupedOpenApi;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springdoc.core.properties.AbstractSwaggerUiConfigProperties;
+import org.springdoc.core.properties.SwaggerUiConfigParameters;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteDefinition;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -24,19 +26,19 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
-	@Autowired
-	RouteDefinitionLocator locator;
-
 	@Bean
-	public List<GroupedOpenApi> apis() {
-		List<GroupedOpenApi> groups = new ArrayList<>();
+	@Lazy(false)
+	public Set<AbstractSwaggerUiConfigProperties.SwaggerUrl> apis(RouteDefinitionLocator locator,
+																  SwaggerUiConfigParameters swaggerUiConfigParameters) {
+		Set<AbstractSwaggerUiConfigProperties.SwaggerUrl> urls = new HashSet<>();
 		List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
-		assert definitions != null;
 		definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-service")).forEach(routeDefinition -> {
 			String name = routeDefinition.getId().replaceAll("-service", "");
-			groups.add(GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build());
+			AbstractSwaggerUiConfigProperties.SwaggerUrl swaggerUrl = new AbstractSwaggerUiConfigProperties.SwaggerUrl(name, "/" + name, null);
+			urls.add(swaggerUrl);
 		});
-		return groups;
+		swaggerUiConfigParameters.setUrls(urls);
+		return urls;
 	}
 
 }
