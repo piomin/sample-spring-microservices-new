3422c3536b9b575474634fe0d99fc07eb4d0b621
khorn-infomedia <khorn@infomedia.com.au>
2025-01-16 13:33:15 +1100

    update to SBoot 3.4.1, Cloud 2024, OpenAI 2.8, Fix Gateway bug

--- gateway-service/src/main/java/pl/piomin/services/gateway/GatewayApplication.java
+++ gateway-service/src/main/java/pl/piomin/services/gateway/GatewayApplication.java
@@ -2,8 +2,9 @@ package pl.piomin.services.gateway;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springdoc.core.properties.AbstractSwaggerUiConfigProperties;
-import org.springdoc.core.properties.SwaggerUiConfigParameters;
+import org.springdoc.core.properties.AbstractSwaggerUiConfigProperties.SwaggerUrl;
+import org.springdoc.core.properties.SwaggerUiConfigProperties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -15,6 +16,9 @@ import org.springframework.context.annotation.Lazy;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
+import static org.springdoc.core.utils.Constants.DEFAULT_API_DOCS_URL;
+
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -28,16 +32,15 @@ public class GatewayApplication {
 
 	@Bean
 	@Lazy(false)
-	public Set<AbstractSwaggerUiConfigProperties.SwaggerUrl> apis(RouteDefinitionLocator locator,
-																  SwaggerUiConfigParameters swaggerUiConfigParameters) {
-		Set<AbstractSwaggerUiConfigProperties.SwaggerUrl> urls = new HashSet<>();
+	public Set<SwaggerUrl> apis(RouteDefinitionLocator locator, SwaggerUiConfigProperties swaggerUiConfigProperties) {
+		Set<SwaggerUrl> urls = new HashSet<>();
 		List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
 		definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-service")).forEach(routeDefinition -> {
 			String name = routeDefinition.getId().replaceAll("-service", "");
-			AbstractSwaggerUiConfigProperties.SwaggerUrl swaggerUrl = new AbstractSwaggerUiConfigProperties.SwaggerUrl(name, "/" + name, null);
+			SwaggerUrl swaggerUrl = new SwaggerUrl(name, DEFAULT_API_DOCS_URL+"/" + name, null);
 			urls.add(swaggerUrl);
 		});
-		swaggerUiConfigParameters.setUrls(urls);
+		swaggerUiConfigProperties.setUrls(urls);
 		return urls;
 	}
 
